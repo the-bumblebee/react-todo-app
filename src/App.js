96ba27e1a58ba540323d9e08245185634eda1147
Asif Nassar <klmktu.asif@gmail.com>
2024-07-14 18:20:54 +0530

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        Your Todo's
-      </div>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th scope='col'>#</th>
-              <th scope='col'>Description</th>
-              <th scope='col'>Assigned</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope='row'>1</th>
-              <td>Feed cat</td>
-              <td>Eric</td>
-            </tr>
-            <tr>
-              <th scope='row'>2</th>
-              <td>Get haircut</td>
-              <td>Eric</td>
-            </tr>
-          </tbody>
-        </table>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Feed cat</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
