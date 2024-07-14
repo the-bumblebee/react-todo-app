1cba48096fa92438f1729e44b9e2d59466b01f47
Asif Nassar <klmktu.asif@gmail.com>
2024-07-14 23:39:46 +0530

    Added form to add todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
@@ -11,12 +12,12 @@ function App() {
     { rowNumber: 4, rowDescription: 'Charge phone', rowAssigned: 'User One' },
   ]);
 
-  const addTodo = () => {
+  const addTodo = (description, assigned) => {
     if (todos.length > 0) {
       const newTodo = {
         rowNumber: todos.length + 1,
-        rowDescription: 'New Todo',
-        rowAssigned: 'User Three'
+        rowDescription: description,
+        rowAssigned: assigned
       };
       setTodos(todos => [...todos, newTodo]);
       console.log(todos);
@@ -34,6 +35,7 @@ function App() {
           <button className="btn btn-primary" onClick={addTodo}>
             Add new todo
           </button>
+          <NewTodoForm addTodo={addTodo} />
         </div>
       </div>
     </div>
---
+++ src/components/NewTodoForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function NewTodoForm(props) {
+
+  const [description, setDescription] = useState("");
+  const [assigned, setAssigned] = useState("");
+
+  const submitTodo = () => {
+    if (description !== "" && assigned !== "") {
+      props.addTodo(description, assigned);
+      setDescription("");
+      setAssigned("");
+    }
+  }
+
+  return (
+    <div className="mt-5">
+      <form action="">
+        <div className="mb-5">
+          <label className="form-label">Assigned</label>
+          <input
+            type="text"
+            className="form-control"
+            required
+            onChange={e => setAssigned(e.target.value)}
+            value={assigned}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            required
+            onChange={e => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={submitTodo}
+        >Add Todo</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewTodoForm;
